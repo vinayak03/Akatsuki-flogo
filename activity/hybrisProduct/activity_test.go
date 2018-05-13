79912e2b65f69c7338d7aea72dc618904efe0101
prshinde <prshinde@tibco.com>
2018-05-13 16:36:10 +0530

    adding CreateProduct Operation in HybrisProduct

--- activity/hybrisProduct/activity_test.go
+++ activity/hybrisProduct/activity_test.go
@@ -90,6 +90,21 @@ func TestDeleteAllProducts(t *testing.T) {
 	t.Log("TestDeleteAllProducts Output\n Status:", status, "\nResponsePayload", payload)
 }
 
+func TestCreateProduct(t *testing.T) {
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	setInput(tc, "CreateProduct", "bvCSvnE0BAYcOLKFAD9GxnSToKIwUUWJ", "http://localhost:7778", "dummy", "1", "{\"metadata\":{\"additionalProperties\":true,\"type\":\"string\",\"properties\":{\"mixins\":{\"additionalProperties\":{\"format\":\"string\",\"type\":\"string\"},\"type\":\"string\"},\"variants\":{\"additionalProperties\":true,\"type\":\"string\",\"properties\":{\"options\":{\"additionalProperties\":{\"format\":\"string\",\"type\":\"string\"},\"type\":\"string\"}}}}},\"id\":\"string\",\"code\":\"string\",\"name\":\"string\",\"description\":\"string\",\"published\":true,\"mixins\":\"string\"}")
+
+	act.Eval(tc)
+
+	status := tc.GetOutput(oValueStatus).(string)
+	payload := tc.GetOutput(oValueResponsePayload)
+
+	assert.Equal(t, "200", status, "Response Code does not match")
+	t.Log("TestCreateProduct Output\n Status:", status, "\nResponsePayload", payload)
+}
+
 func TestUpdateProduct(t *testing.T) {
 	act := NewActivity(getActivityMetadata())
 	tc := test.NewTestActivityContext(getActivityMetadata())
@@ -103,6 +118,21 @@ func TestUpdateProduct(t *testing.T) {
 
 	assert.Equal(t, "200", status, "Response Code does not match")
 	t.Log("TestUpdateProduct Output\n Status:", status, "\nResponsePayload", payload)
+}
+
+func TestGetProductFail(t *testing.T) {
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	setInput(tc, "GetProduct", "bvCSvnE0BAYcOLKFAD9GxnSToKIwUUWJ", "http://localhost:7779", "dummy", "product1", "")
+
+	act.Eval(tc)
+
+	status := tc.GetOutput(oValueStatus).(string)
+	payload := tc.GetOutput(oValueResponsePayload)
+
+	assert.Equal(t, "500", status, "Response Code does not match")
+	t.Log("TestGetProduct Output\n Status:", status, "\nResponsePayload", payload)
 }
 
 func setInput(tc *test.TestActivityContext, requestType string, APIKey string, url string, tenant string, productId string, body string) {
